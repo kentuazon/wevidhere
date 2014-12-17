3e2bcb0fe422cd2b1812f309ad83f60542d6d4b2
Ken Tuazon <kentuazon@Kens-MacBook-Pro.local>
2014-12-17 12:22:01 -0800

    put all up

--- public/js/ag-controllers.js
+++ public/js/ag-controllers.js
@@ -1,15 +1,26 @@
-var WeVidHereConfig = angular.module('WeVidHere', []);
+var WeVidHereConfig = angular.module('WeVidHere',[]);
 
 WeVidHereConfig.controller(
 	'WeVidHereConfig',
 	[
-		'$scope', //the object we attach verything to
-		'$http',   // imported Ajax object, injected into out controller
+		'$scope', //the object we attach everything to
+		'$http',   //imported Ajax object, injected into our controller
 		function ($scope, $http) {
-			
+
+			//get data by ajax
+			$http(
+				method:"GET",
+				url:"data/users.json"
+			)
+			.success(function (data, status, headers, config) {
+				//do something if we got the data
+				}
+			);
+		
+			//check data on submit form
+			$scope.doLogin=function() {
+				//process what user typed in
+			}
 		}
-	]
-
-
-
-);
+	] 
+); //end WeVidHereConfig.controller
